a90031855e25ad1947514d90f9a3c80d3914323d
jungyu <jungyu@naver.com>
2022-06-19 17:23:53 +0900

    logout router

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const { User } = require("../models/User");
 
-let auth = (res, req, next) => {
+let auth = (req, res, next) => {
   let token = req.cookies.x_auth;
 
   User.findByToken(token, (err, user) => {
