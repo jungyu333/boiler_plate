301d79cc73d568e2b65ab4d9f286d0300eecf290
jungyu <jungyu@naver.com>
2022-06-19 15:06:46 +0900

    Setting

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://jungyu:rlawnsrb1@boilerplate.wrdto.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!@!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
